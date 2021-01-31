906bbe0e60681821d50429db1c67c37a4a602593
Eunice Fiorella <euniceancajima@gmail.com>
2021-01-30 21:11:07 -0500

    Maquetación de la pagina index

--- src/main.js
+++ src/main.js
@@ -3,4 +3,15 @@ import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
+const navEventos=()=>{
+    const burger=document.querySelector('.burger');
+    const nav=document.querySelector('.nav');
+    
+    burger.addEventListener('click',function(){
+        burger.classList.toggle('move');
+        nav.classList.toggle('move');
+    });
+}
+
+navEventos();
 console.log(example, data);
