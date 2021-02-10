190c39facbedac3f7a23d71e0bdeeafd2495c233
Eunice Fiorella <euniceancajima@gmail.com>
2021-02-10 11:28:17 -0500

    Testeo de algunas funciones de data.js

--- src/data.js
+++ src/data.js
@@ -1,68 +1,71 @@
-export const searchPokemon = (obj,dato) => {
-  const resultSearch=obj.filter(item=>
-    item.name.startsWith(dato.toLowerCase()));
+export const searchPokemon = (obj, dato) => {
+  const resultSearch = obj.filter((item) =>
+    item.name.startsWith(dato.toLowerCase())
+  );
   return resultSearch;
 };
-export const orderAlpha = (option,arrayShow)=>{ 
-  switch(option){
-    case '1':
-       arrayShow.sort((a, b) => a.name.localeCompare(b.name));
+export const orderAlpha = (option, arrayShow) => {
+  switch (option) {
+    case "1":
+      arrayShow.sort((a, b) => a.name.localeCompare(b.name));
       break;
-    case '2':
+    case "2":
       arrayShow.sort((a, b) => a.name.localeCompare(b.name));
       arrayShow.reverse();
-      break;  
+      break;
   }
   return arrayShow;
 };
-export const orderRegion = (option,array) => {
-  let arrayRegion=[];
-  switch(option){
-    case '1':
-        arrayRegion=array;
+export const orderRegion = (option, array) => {
+  let arrayRegion = [];
+  switch (option) {
+    case "1":
+      arrayRegion = array;
       break;
-    case '2':
-          arrayRegion=array.filter(item=>
-          item.generation['name']=='kanto');
-        break;
-        case '3':
-            arrayRegion=array.filter(item=>
-              item.generation['name']=='johto');
-        break;
+    case "2":
+      arrayRegion = array.filter((item) => item.generation["name"] == "kanto");
+      break;
+    case "3":
+      arrayRegion = array.filter((item) => item.generation["name"] == "johto");
+      break;
   }
   return arrayRegion;
 };
 
 // Filtrar por CP
-export const orderByMxCP=(option,arrayShow)=>{ 
-   switch(option){
-    case '1':
-      arrayShow.sort((a, b) => b.stats['max-cp'] - a.stats['max-cp']);
+export const orderByMxCP = (option, arrayShow) => {
+  switch (option) {
+    case "1":
+      arrayShow.sort((a, b) => b.stats["max-cp"] - a.stats["max-cp"]);
       break;
-    case '2':
-      arrayShow.sort((a, b) => b.stats['max-cp'] - a.stats['max-cp']);
+    case "2":
+      arrayShow.sort((a, b) => b.stats["max-cp"] - a.stats["max-cp"]);
       arrayShow.reverse();
-      break;  
+      break;
   }
   return arrayShow;
 };
 
 // Filtrar pokemon por Tipo
 export const filterByType = (obj, property, condition) => {
-    const arrayType=obj.filter(element => (element[property][0] === condition
-  || element[property][1] === condition));
+  const arrayType = obj.filter(
+    (element) =>
+      element[property][0] === condition || element[property][1] === condition
+  );
   return arrayType;
 };
 
 // Top 10
-export const filterTopshow=(pokemonList)=>{
-    pokemonList.sort((a,b)=>a['spawn-chance']-b['spawn-chance']);
-    pokemonList.reverse();
+export const filterTopshow = (pokemonList) => {
+  pokemonList.sort((a, b) => a["spawn-chance"] - b["spawn-chance"]);
+  pokemonList.reverse();
   return pokemonList;
-}
+};
 
 // Funcion para evolucion
-export function computeStats(obj, condition) { 
-  const nameFilter=obj.filter(compare => (compare.name === condition));
+export function computeStats(obj, condition) {
+  const nameFilter = obj.filter(
+    (compare) => compare.name === condition.toLowerCase()
+  );
   return nameFilter;
 }
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,22 +1,208 @@
-import { searchPokemon } from '../src/data.js';
+import { searchPokemon, filterByType, filterTopshow } from "../src/data.js";
 
-describe('search', () => {
-  it('is a function', () => {
-    expect(typeof searchPokemon).toBe('function');
+const data = [
+  {
+    num: "001",
+    name: "bulbasaur",
+    generation: {
+      name: "kanto",
+    },
+    type: ["grass", "poison"],
+    "spawn-chance": "0.69",
+    stats: {
+      "max-cp": "1115",
+    },
+  },
+  {
+    num: "002",
+    name: "ivysaur",
+    generation: {
+      name: "kanto",
+    },
+    type: ["grass", "poison"],
+    "spawn-chance": "0.042",
+    stats: {
+      "max-cp": "1699",
+    },
+  },
+  {
+    num: "004",
+    name: "charmander",
+    generation: {
+      name: "kanto",
+    },
+    type: ["fire"],
+    "spawn-chance": "0.253",
+    stats: {
+      "max-cp": "980",
+    },
+  },
+  {
+    num: "152",
+    name: "chikorita",
+    generation: {
+      name: "johto",
+    },
+    type: ["grass"],
+    "spawn-chance": "3.6",
+    stats: {
+      "max-cp": "935",
+    },
+  },
+  {
+    num: "153",
+    name: "bayleef",
+    generation: {
+      name: "johto",
+    },
+    type: ["grass"],
+    "spawn-chance": "1.31",
+    stats: {
+      "max-cp": "1454",
+    },
+  },
+  {
+    num: "154",
+    name: "meganium",
+    generation: {
+      name: "johto",
+    },
+    type: ["fire"],
+    "spawn-chance": "2.5",
+    stats: {
+      "max-cp": "2410",
+    },
+  },
+];
+
+const search = [
+  {
+    num: "004",
+    name: "charmander",
+    generation: {
+      name: "kanto",
+    },
+    type: ["fire"],
+    "spawn-chance": "0.253",
+    stats: {
+      "max-cp": "980",
+    },
+  },
+  {
+    num: "152",
+    name: "chikorita",
+    generation: {
+      name: "johto",
+    },
+    type: ["grass"],
+    "spawn-chance": "3.6",
+    stats: {
+      "max-cp": "935",
+    },
+  },
+];
+
+const topResult = [
+  {
+    num: "152",
+    name: "chikorita",
+    generation: {
+      name: "johto",
+    },
+    type: ["grass"],
+    "spawn-chance": "3.6",
+    stats: {
+      "max-cp": "935",
+    },
+  },
+  {
+    num: "154",
+    name: "meganium",
+    generation: {
+      name: "johto",
+    },
+    type: ["fire"],
+    "spawn-chance": "2.5",
+    stats: {
+      "max-cp": "2410",
+    },
+  },
+  {
+    num: "153",
+    name: "bayleef",
+    generation: {
+      name: "johto",
+    },
+    type: ["grass"],
+    "spawn-chance": "1.31",
+    stats: {
+      "max-cp": "1454",
+    },
+  },
+  {
+    num: "001",
+    name: "bulbasaur",
+    generation: {
+      name: "kanto",
+    },
+    type: ["grass", "poison"],
+    "spawn-chance": "0.69",
+    stats: {
+      "max-cp": "1115",
+    },
+  },
+  {
+    num: "004",
+    name: "charmander",
+    generation: {
+      name: "kanto",
+    },
+    type: ["fire"],
+    "spawn-chance": "0.253",
+    stats: {
+      "max-cp": "980",
+    },
+  },
+  {
+    num: "002",
+    name: "ivysaur",
+    generation: {
+      name: "kanto",
+    },
+    type: ["grass", "poison"],
+    "spawn-chance": "0.042",
+    stats: {
+      "max-cp": "1699",
+    },
+  },
+];
+
+describe("search", () => {
+  it("is a function", () => {
+    expect(typeof searchPokemon).toBe("function");
   });
 
-  // it('returns `example`', () => {
-  //   expect(example()).toBe('example');
+  it("Retorna los resultados de la busqueda `searchPokemon`", () => {
+    expect(searchPokemon(data, "ch")).toEqual(search);
+  });
+});
+
+describe("anotherExample", () => {
+  it("is a function", () => {
+    expect(typeof filterByType).toBe("function");
+  });
+
+  // it(" `filterByType`", () => {
+  //   expect(filterByType()).toBe("OMG");
   // });
 });
 
+describe("Filtrar por top de frecuencia de aparición", () => {
+  it("is a function", () => {
+    expect(typeof filterTopshow).toBe("function");
+  });
 
-// describe('anotherExample', () => {
-//   it('is a function', () => {
-//     expect(typeof anotherExample).toBe('function');
-//   });
-
-//   it('returns `anotherExample`', () => {
-//     expect(anotherExample()).toBe('OMG');
-//   });
-// });
+  it(" `filterTopshow`", () => {
+    expect(filterTopshow(data)).toEqual(topResult);
+  });
+});
